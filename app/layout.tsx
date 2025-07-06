a470bdb5a4f97db981037f67e471e0909630b1fc
AbolareRoheemah <roheemahabo@gmail.com>
2025-07-06 22:33:15 +0100

    removed use effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
 import Header from "./components/Header";
-import { useEffect } from "react";
 
 export const viewport: Viewport = {
   width: "device-width",
